8775b10b8e2c9f5730208072b16d31652efaaab6
Alejandro Lopez <program.alopez@gmail.com>
2023-06-10 14:43:20 -0500

    fix: rename initial node for render

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import {App} from './App';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('alx-react') as HTMLElement
 );
 root.render(
   <React.StrictMode>
