5d707c466c4c143a631ab4cc1dd4a8708ee6c0c2
Alejandro Lopez <program.alopez@gmail.com>
2023-06-09 20:31:01 -0500

    fix: es translations

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ export function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edita <code>src/App.tsx</code> guarda para recargar.
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ export function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprende React
         </a>
       </header>
     </div>
